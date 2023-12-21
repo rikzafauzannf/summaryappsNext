db0bc9e4da905f0eec4344b01d7797e0d310fbd7
rikzafauzannf <rikzarazor491@gmail.com>
2023-12-21 12:23:08 +0700

    update

--- src/app/api/getAPi.js
+++ src/app/api/getAPi.js
@@ -1,13 +1,16 @@
-export const responseBageur = await fetch(process.env.SECRET_BAGEUR_API, {
+export const responseBageur = await fetch(process.env.NEXT_PUBLIC_BAGEUR_API, {
   cache: "force-cache",
 });
 
-export const responseSetaman = await fetch(process.env.SECRET_SETAMAN_SEMAI, {
-  cache: "force-cache",
-});
+export const responseSetaman = await fetch(
+  process.env.NEXT_PUBLIC_SETAMAN_SEMAI,
+  {
+    cache: "force-cache",
+  }
+);
 
 export const responseSetamanTanam = await fetch(
-  process.env.SECRET_SETAMAN_TANAM,
+  process.env.NEXT_PUBLIC_SETAMAN_TANAM,
   {
     cache: "force-cache",
   }

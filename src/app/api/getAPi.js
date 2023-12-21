64d97aeb4595dad1bf72ebd6749129a21bcdad20
rikzafauzannf <rikzarazor491@gmail.com>
2023-12-21 11:48:04 +0700

    update process api

--- src/app/api/getAPi.js
+++ src/app/api/getAPi.js
@@ -1,14 +1,14 @@
 export const responseBageur = await fetch(process.env.SECRET_BAGEUR_API, {
-  cache: "no-store",
+  cache: "force-cache",
 });
 
 export const responseSetaman = await fetch(process.env.SECRET_SETAMAN_SEMAI, {
-  cache: "no-store",
+  cache: "force-cache",
 });
 
 export const responseSetamanTanam = await fetch(
   process.env.SECRET_SETAMAN_TANAM,
   {
-    cache: "no-store",
+    cache: "force-cache",
   }
 );

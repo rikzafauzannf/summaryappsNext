ad458437cff7cd8a16590f1828f2cb621ed3ed2e
rikzafauzannf <rikzarazor491@gmail.com>
2023-12-21 11:36:11 +0700

    up

--- src/app/bageur/page.jsx
+++ src/app/bageur/page.jsx
@@ -19,7 +19,7 @@ export default async function Bageur() {
     return (
       <>
         <h1 className="text-2xl font-bold mb-3">Dashboard Bageur</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
           {data.map((item) => {
             return (
               <CardBageur

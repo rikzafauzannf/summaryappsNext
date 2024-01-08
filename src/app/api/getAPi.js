5ac276b959401aa77e770211d52b9fa3cb373a43
rikzafauzannf <rikzarazor491@gmail.com>
2024-01-08 14:45:19 +0700

    set

--- src/app/api/getAPi.js
+++ src/app/api/getAPi.js
@@ -1,12 +1,20 @@
-import { Axios } from "axios";
-export const responseBageur = await Axios.get(
-  `${process.env.NEXT_PUBLIC_BAGEUR_API}`
+export const responseBageur = await fetch(
+  `${process.env.NEXT_PUBLIC_BAGEUR_API}`,
+  {
+    cache: "force-cache",
+  }
 );
 
-export const responseSetaman = await Axios.get(
-  `${process.env.NEXT_PUBLIC_SETAMAN_SEMAI}`
+export const responseSetaman = await fetch(
+  `${process.env.NEXT_PUBLIC_SETAMAN_SEMAI}`,
+  {
+    cache: "force-cache",
+  }
 );
 
-export const responseSetamanTanam = await Axios.get(
-  `${process.env.NEXT_PUBLIC_SETAMAN_TANAM}`
+export const responseSetamanTanam = await fetch(
+  `${process.env.NEXT_PUBLIC_SETAMAN_TANAM}`,
+  {
+    cache: "force-cache",
+  }
 );
